e77a664e48a13893950f73160642614ce844a4ac
Amila Indrajith <e12351@ce.pdn.ac.lk>
2017-02-25 12:23:59 +0530

    Add files via upload
    node app.js <usb port> <server port>

---
+++ App.js
@@ -0,0 +1,137 @@
+/*Http server*/
+var express = require("express");
+var app = express();
+var http = require('http').Server(app);
+var path = __dirname + '/';
+var io = require('socket.io')(http);
+var SerialPort = require('serialport');
+var prompt = require('prompt');
+var bodyParser = require('body-parser');
+var myip = require('quick-local-ip');
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+var info="";
+var res="";
+
+var FLAG_1 = '0';
+var FLAG_2 = '0';
+var FLAG_3 = '0';
+var FLAG_4 = '0';
+var FLAG_5 = '0';
+var FLAG_6 = '0';
+
+var USB = '';
+var PORT = ''
+// function getinputs() {
+//   prompt.start();
+//   prompt.get(['username', 'email'], function (err, result) {
+//       console.log('Command-line input received:');
+//       console.log('  username: ' + result.username);
+//       console.log('  email: ' + result.email);
+//   });
+// }
+
+// console.log(myip.getLocalIP4());
+
+if (process.argv[2] == null) {
+  console.log('\nERROR : you did not enter the usb port.\n   node app.js <Arduino port> <server port>')
+  process.exit();
+};
+
+PORT = process.argv[3];
+if (process.argv[3] == null) {
+  PORT = '4000';
+  console.log('\nWARNING : you did not enter the server port\n    node app.js <Arduino port> <server port>\n.    default server port is 4000.\n\n')
+}
+
+USB = process.argv[2];
+// console.log('Arduino port          : '+USB)
+// console.log('Server listening port : '+PORT)
+
+var port = new SerialPort(USB, {
+  baudrate: 9600,
+  bufferSize: 1 ,
+  rtscts: true ,
+});
+
+port.on('data', function (data) {
+  var data_R = data.toString('utf8');
+  if (data_R == 'D'){
+    io.emit('data', res);
+    // console.log(res);
+    res = '';
+  }
+  res = res.concat(data_R);
+});
+
+io.on('connection', function(socket){
+  console.log('User connected');
+  socket.on('message', function(msg){
+    // console.log('message: ' + msg);
+    if((FLAG_1 == '0') && (msg == '1')){
+      FLAG_1 = '1';
+      port.write('1');
+    }else if((FLAG_1 == '1') && (msg == '1')){
+      FLAG_1 = '0';
+      port.write('7');
+    }
+
+    if((FLAG_2 == '0') && (msg == '2')){
+      FLAG_2 = '1';
+      port.write('2');
+    }else if((FLAG_2 == '1') && (msg == '2')){
+      FLAG_2 = '0';
+      port.write('8');
+    }
+
+    if((FLAG_3 == '0') && (msg == '3')){
+      FLAG_3 = '1';
+      port.write('3');
+    }else if((FLAG_3 == '1') && (msg == '3')){
+      FLAG_3 = '0';
+      port.write('9');
+    }
+
+    if((FLAG_4 == '0') && (msg == '4')){
+      FLAG_4 = '1';
+      port.write('4');
+    }else if((FLAG_4 == '1') && (msg == '4')){
+      FLAG_1 = '0';
+      port.write('10');
+    }
+
+    if((FLAG_5 == '0') && (msg == '5')){
+      FLAG_5 = '1';
+      port.write('5');
+    }else if((FLAG_5 == '1') && (msg == '5')){
+      FLAG_5 = '0';
+      port.write('11');
+    }
+
+    if((FLAG_6 == '0') && (msg == '6')){
+      FLAG_6 = '1';
+      port.write('6');
+    }else if((FLAG_6 == '1') && (msg == '6')){
+      FLAG_6 = '0';
+      port.write('12');
+    }
+    
+  });
+});
+
+/*Create http server*/
+app.get('/', function(req, res){
+  // console.log(res)
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(PORT, function(){
+  console.log('--------------------------------------------------------------');
+  console.log('-------------------------Loading Server-----------------------');
+  console.log('--------------------------------------------------------------\n');
+  console.log('Server IP : '+myip.getLocalIP4()+'  Port : '+PORT);
+
+});
+
+// setTimeout(main, 10000);
---
+++ Arduino_code/Arduino_code.ino
@@ -0,0 +1,102 @@
+int x; //Switch CASE
+int y = 50; //IO Scan Delay
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(2, INPUT);
+    pinMode(3, INPUT);
+    pinMode(4, INPUT);
+    pinMode(5, INPUT);
+    pinMode(6, INPUT);
+    pinMode(7, INPUT);
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(11, OUTPUT);
+    pinMode(12, OUTPUT);
+    pinMode(13, OUTPUT);
+}
+
+void loop() {
+  while (Serial.available()) {
+      int x = Serial.parseInt();
+//      Serial.println(x);
+        switch (x){
+        case 1:
+        digitalWrite(8,HIGH);
+        break;
+        case 2:
+        digitalWrite(9,HIGH);
+        break;
+        case 3:
+        digitalWrite(10,HIGH);
+        break; 
+        case 4:
+        digitalWrite(11,HIGH);
+        break;
+        case 5:
+        digitalWrite(12,HIGH);
+        break;
+        case 6:
+        digitalWrite(13,HIGH);
+        break;
+        case 7:
+        digitalWrite(8,LOW);
+        break;
+        case 8:
+        digitalWrite(9,LOW);
+        break;
+        case 9:
+        digitalWrite(10,LOW);
+        break;
+        case 10:
+        digitalWrite(11,LOW);
+        break;
+        case 11:
+        digitalWrite(12,LOW);
+        break;
+        case 12:
+        digitalWrite(13,LOW);
+        break;
+      }
+  }
+  
+  Serial.print("DI 1 = ");
+  Serial.println(digitalRead(2));
+  delay(y);
+  Serial.print("DI 2 = ");
+  Serial.println(digitalRead(3));
+  delay(y);
+  Serial.print("DI 3 = ");
+  Serial.println(digitalRead(4));
+  delay(y);
+  Serial.print("DI 4 = ");
+  Serial.println(digitalRead(5));
+  delay(y);
+  Serial.print("DI 5 = ");
+  Serial.println(digitalRead(6));
+  delay(y);
+  Serial.print("DI 6 = ");
+  Serial.println(digitalRead(7));
+  delay(y);
+  Serial.print("DO 1 = ");
+  Serial.println(digitalRead(8));
+  delay(y);
+  Serial.print("DO 2 = ");
+  Serial.println(digitalRead(9));
+  delay(y);
+  Serial.print("DO 3 = ");
+  Serial.println(digitalRead(10));
+  delay(y);
+  Serial.print("DO 4 = ");
+  Serial.println(digitalRead(11));
+  delay(y);
+  Serial.print("DO 5 = ");
+  Serial.println(digitalRead(12));
+  delay(y);
+  Serial.print("DO 6 = ");
+  Serial.println(digitalRead(13));
+  delay(y);
+  
+  delay(1000);
+}
